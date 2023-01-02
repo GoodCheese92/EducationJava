f57f87dcb258ed287b18636ff1cddeddec7267a0
GoodCheese92 <dltjdwls555@gmail.com>
2023-01-02 15:49:50 +0900

    국비지원 2023-01-02

--- Ex_0102/src/ex2_value_type/Study_230102.java
+++ Ex_0102/src/ex2_value_type/Study_230102.java
@@ -15,7 +15,7 @@ public class Study_230102 {
 		int regNo2 = 111111;
 		System.out.println(regNo1 + "-" + regNo2);
 		long regNo = 9203241111111L;
-		System.out.println(regNo);		// 주민등록 번호는 
+		System.out.println(regNo);		// 주민등록 번호는 등록
 		
 		// 2-3 리터럴, 변수, 키워드
 		int i = 100;
@@ -37,22 +37,54 @@ public class Study_230102 {
 		System.out.println('1' + '2');		// 99
 		System.out.println("J" + "ava");	// Java
 		//System.out.println(true + null);	// 오류
+		// char, byte, short 같은 경우 덧셈 연산이 될 때 int형 정수값으로 변환된다.
+		// 문자열 + any type이 되면 다 문자열로 변환된다.
+		// but, 덧셈연산이 먼저 나올 경우, 덧셈이 먼저 진행 ex) 1 + 1 + "1" -> 21, "1"+ 1 + 1 -> 111
+		System.out.println(1+1+"1");
+		System.out.println("1"+1+1);
 		
-		// 2-6 키워드? a, d, e
+		// 2-6 키워드가 아닌 것은? b, c, d, e
+		// ex) 키워드 예시 - abstract, if, for, true, throws, fianl, null, void
 		
 		// 2-7 변수 이름으로 사용 가능한 것? c, d, e, f
+		// (1) 대소문자 구별, 길이 상관 없음
+		// (2) 예약어 사용 불가 - ex) true는 불가, True는 가능
+		// (3) 숫자로 시작해서는 안된다.
+		// (4) 특수문자는 '_'와 '$'만을 허용한다.
+		
 		
 		// 2-8 참조형 변수(reference type)와 같은 크기의 기본형(primitive type)은? 	a, d
+		// 참조형 변수(reference type)는 기본형(primitive type)을 제외하면 모두 다 이다.
+		// 모든 참조형 변수의 크기는 4byte이다.
 		
-		// 2-9 형변환 생략 가능? 	b, d
+		// **2-9 형변환 생략 가능? 	d, e
 		
-		// 2-10 char 타입의 정수 값의 범위? -128 ~ 127
+		// 2-10 char 타입의 정수 값의 범위? char는 2byte이므로 16비트이고 2^16개의 문자를 표현 가능
 		
-		// 2-11 변수 잘못 초기화 한 것? a, c, e?
+		// 2-11 변수 잘못 초기화 한 것? a, b, c, d
+		// a. byte b = 256;		// byte의 범위(-128~127)를 넘는 값으로 초기화 할 수 없음.
+		// b. char c = '';		// char는 반드시 한 개의 문자를 지정해야 함
+		// c. char answer = 'no';	// char는 두 개의 문자를 저장할 수 없음
+		// d. float f = 3.14;		// 3.14는 3.14d의 생략된 형태. 접미사 f를 붙이거나 형변환 필요.
 		
-		// 2-12 main 메서드? a
+		double d = 1.4e4;
+		System.out.println(d);
 		
-		// 2-13 타입과 기본값이 잘못? b, 
+		// 2-12 main 메서드? a, b, c, e
+		// public static void main(String[] args)
+		// public static void main(String args[])
+		// public static void main(String[] arv)
+		// static public void main(String[] args)
+		// []의 위치는 타입 또는 변수명 뒤에 붙여도 가능, 변수명은 바뀌어도 상관 없음, static과 public의 위치는 변경 가능
+		
+		
+		// 2-13 타입과 기본값이 잘못? c, e, f
+		// a. boolean = false : 기본값
+		// b. char = '￦u0000' : 기본값
+		// c. float = 0.0		// float는 0.0f가 기본값, 0.0은 0.0d에서 접미사 d가 생략된 것.
+		// d. int = 0 : 기본값
+		// e. long = 0 		// long은 0L이 기본값
+		// f. String = "" 	// String은 참조형 타입. 모든 참조형 타입의 기본 값은 null, 크기는 4byte
 		
 		
 	}
---
+++ Ex_0102/src/ex2_value_type/Study_230102_chapter3.java
@@ -0,0 +1,104 @@
+package ex2_value_type;
+
+public class Study_230102_chapter3 {
+	public static void main(String[] args) {
+		// 3-1 연산의 결과를 적으시오.
+		System.out.println("====3-1====");
+		int x = 2;
+		int y = 5;
+		char c = 'A';	// 'A'의 문자코드는 65
+		
+		System.out.println(1+x << 33);				// 6
+		System.out.println(y >= 5 || x < 0 && x>2);	// true
+		System.out.println(y += 10 - x++);			// 13
+		System.out.println(x+=2);					// 5
+		System.out.println(!('A'<= c && c <='Z'));	// false
+		System.out.println('C'-c);					// 2
+		System.out.println('5'-'0');				// 5
+		System.out.println(c+1);					// 66
+		System.out.println(++c);					// B
+		System.out.println(c++);					// B
+		System.out.println(c);						// C
+		System.out.println(x+1);
+		System.out.println(x);
+		
+		// 3-2 사과 바구니 담기
+		System.out.println("====3-2====");
+		int numOfApples = 123;	// 사과의 개수
+		int sizeOfBucket = 10;	// 바구니의 크기(바구니에 담을 수 있는 사과의 개수)
+		int numOfBucket = ((numOfApples/sizeOfBucket) + ((numOfApples%sizeOfBucket>0) ? 1 : 0));
+		System.out.println("필요한 바구니의 수 : "+numOfBucket);
+		
+		// 3-3 '양수', '음수', '0' 출력
+		System.out.println("====3-3====");
+		int num = -2;
+		System.out.println((num > 0) ? "양수": (num < 0 ? "음수" : "0"));
+		
+		// 3-4 백의 자리 이하 버림
+		System.out.println("====3-4====");
+		int num1 = 456;
+		System.out.println((num1/100) * 100);
+		
+		// 3-5 일의 자리 1로 변경
+		System.out.println("====3-5====");
+		int num2 = 333;
+		System.out.println((num2/10)*10 + 1);
+		
+		// 3-6 가장 가까운 10의 배수에서 빼기
+		System.out.println("====3-6====");
+		int num3 = 24;
+		System.out.println(10 - num3%10);
+		//System.out.println((num3/10 + 1)*10 - num3);
+		
+		// **3-7 화씨 온도 -> 섭씨 온도로 변경하기
+		System.out.println("====3-7====");
+		int fahrenheit = 100;
+		float celcius =(5f/9f *((float)fahrenheit-32.0f));
+		System.out.println("Fahrenheit : "+fahrenheit);
+		System.out.println("Celcius : "+celcius);
+		
+		// 3-8 오류 변경하기
+		System.out.println("====3-8====");
+		byte a = 10;
+		byte b = 20;
+		byte c1 = (byte)(a + b);
+		
+		char ch = 'A';
+		ch = (char)(ch + 2);
+		
+		float f = (3f/2);
+		long l = 3000 * 3000 * 3000L;
+		
+		float f2 = 0.1f;
+		double d = 0.1;
+		
+		boolean result = (float)d==f2;
+		
+		System.out.println("c1 = "+c1);
+		System.out.println("ch = "+ch);
+		System.out.println("f = "+f);
+		System.out.println("l = "+l);
+		System.out.println("result = "+result);
+		
+		// 3-9 ch9가 영문자거나 숫자일 때만 b9 값 true
+		System.out.println("====3-9====");
+		char ch9 = 'z';
+		
+		boolean b9 = ((ch9>= 48 && ch9<=57)||(ch9>= 65 && ch9<=90)||(ch9>= 97 && ch9<=122));
+		
+		System.out.println(b9);
+		
+		// 3-10 대문자 -> 소문자 변경
+		System.out.println("====3-10====");
+		char ch10 = 'A';
+		
+		char lowerCase = (ch10>=65 && ch10<=90) ? ((char)(ch10+32)) : ch10;
+		
+		System.out.println("ch10 : "+ ch10);
+		System.out.println("ch10 to lowerCase : "+lowerCase);
+		
+		
+	
+	
+	}
+}
