3a452617024178a4b7e8375f1899b164dbf45a6f
GoodCheese92 <dltjdwls555@gmail.com>
2023-01-05 15:39:52 +0900

    국비지원 2023-01-05

--- Ex_0104/src/ex8_work/Ex1_work.java
+++ Ex_0104/src/ex8_work/Ex1_work.java
@@ -23,13 +23,15 @@ public class Ex1_work {
 		//     * * * * *
 		//   * * * * * * *
 		// * * * * * * * * *
-		int a = 1;
 		for (int i = 1; i <= 5; i++) {
-			for (int j = 1; j <= 9; j++) {
-				System.out.print("* ");
+			for (int j = 0; j < i*2-1; j++) {
+				if(j%2==0){
+					System.out.print("*");					
+				}else {
+					System.out.print(" ");
+				}
 			}
 			System.out.println();
-			a += 2;
 		}
 
 	} // end of main
